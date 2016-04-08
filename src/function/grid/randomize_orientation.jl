0e9c3a2365a541fe307045f3a16c0a96bf40b03c
Yuhang(Steven) Wang <me@me>
2016-04-07 19:56:24 -0500

    add randomize_orientation.jl

--- src/function/grid/randomize_orientation.jl
+++ src/function/grid/randomize_orientation.jl
@@ -1,6 +1,5 @@
 """
-Randomize the orientation of the molecule
-at each grid point.
+Rotate the input vector to a random orientation.
 
 Arguments
 ----------
@@ -15,5 +14,5 @@ seed=0:UInt32
     If non-zero, this seed will be used for the random number
     generator.
 """ 
-function randomize(object_array::Array, array_size::Tuple; seed::UInt32=0)
+function randomize_orientation(object_array::Array, array_size::Tuple; seed::UInt32=0)
 end
