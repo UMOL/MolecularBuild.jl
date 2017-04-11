95f51170a5e47091dcb008f917fa69836b874ee6
yuhangwang <stevenaura@outlook.com>
2017-04-10 20:07:13 -0500

    rm src/Fn

--- src/Fn/Fn.jl
+++
@@ -1,8 +0,0 @@
-module Fn
-include(joinpath("Toolkit","Toolkit.jl"))
-include(joinpath("Align","Align.jl"))
-include(joinpath("Build","Build.jl"))
-
-using Build
-export build
-end
--- src/Fn/sphere/all.jl
+++
@@ -1,2 +0,0 @@
-include("build_FibonacciSphere.jl")
-include("build_PartialFibonacciSphere.jl")
